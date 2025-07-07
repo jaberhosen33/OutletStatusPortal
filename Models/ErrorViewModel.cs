393563845420307f9323839ce2e39b87fc95ef82
jaberhosen33 <jaberhosen33@gmail.com>
2025-07-07 14:03:55 +0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OutletStatusPortal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
